8b4ae6d8756fd5f425507e4508b8488d371b9217
lichengzhou <1026290508@qq.com>
2022-09-05 17:28:40 +0800

    测试

--- web-category/src/main/java/com/qf/ec/category/controller/TestController.java
+++ web-category/src/main/java/com/qf/ec/category/controller/TestController.java
@@ -1,16 +1,20 @@
 package com.qf.ec.category.controller;
 
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Api("商品分类api")
-@Slf4j      //log
+@Slf4j//log
 @RestController
 @RequestMapping("/test")
 public class TestController {
+
+
+    @ApiOperation("获取商品信息")
     @GetMapping("/list")
     public String list(){
         return "测试list";
